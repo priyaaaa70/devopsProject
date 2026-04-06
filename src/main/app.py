295a2bd9ef8a4c78c5eec1dcabb40001bdd1892a
Priya Agrawal <priyaagrawal8305@gmail.com>
2026-04-07 05:06:24 +0530

    test webhook

--- src/main/app.py
+++ src/main/app.py
@@ -8,7 +8,7 @@ df = pd.read_csv("pipeline_data.csv")
 
 # ---------------- HEADER ----------------
 st.title("CI/CD Pipeline Performance Benchmarker")
-st.markdown("Analyze and compare Jenkins pipeline performance across teams")
+st.markdown("Real-time CI/CD Pipeline Performance Benchmarker | Priya Agrawal | IIS3250")
 
 st.divider()
 
