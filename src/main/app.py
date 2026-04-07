b0b5b31b4af7efef8556a857c3b6de5a23d7df9a
Priya Agrawal <priyaagrawal8305@gmail.com>
2026-04-07 20:57:33 +0530

    updated frontend

--- src/main/app.py
+++ src/main/app.py
@@ -8,7 +8,7 @@ df = pd.read_csv("pipeline_data.csv")
 
 # ---------------- HEADER ----------------
 st.title("CI/CD Pipeline Performance Benchmarker")
-st.markdown("Real-time CI/CD Pipeline Performance Benchmarker | Priya")
+st.markdown("Real-time CI/CD Pipeline Performance Benchmarker")
 
 st.divider()
 
